bc1e442fc7154a9897cbd1242a933bd56ebb4c8a
Himanshu <yadavhimanshu63@gmail.com>
2020-08-28 18:49:55 +0530

    Monthly Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,34 +1,43 @@
 public class EmployeeWage
 {
-         //constants
-	private final int ISFULLTIME=1;
-	private final int ISPARTTIME=2;
+        //constants
+	private  final int IS_FULL_TIME = 1;
+	private  final int IS_PART_TIME = 2;
 	
 	//variables
-	private final int empRate;
+	private  final int empRatePerHour;
+	private  final int totalDays;
 	
-    public EmployeeWage(int empRate)
-    {
-    	this.empRate=empRate;
-    }
-    
+	public EmployeeWage(int empRatePerHour , int totalDays)
+	{
+		this.empRatePerHour=empRatePerHour;
+		this.totalDays=totalDays;
+	}
+	
+	//method to calculate wage
 	public void empWage() {
+		int totalEmpWage=0;
+		for ( int day=1 ; day<=totalDays ; day++)
+		{
 		int empHour = empCheck();
-		int empWage = empHour * empRate;
-		System.out.println("Employee Wage: " + empWage);
+		int empWage = empHour * empRatePerHour;
+		totalEmpWage=totalEmpWage+empWage;
+		System.out.println("day: "+ day + " Employee Wage: "+ empWage);
+		}
+		System.out.println( "Total Employee Wage: " + totalEmpWage);
 	}
 
+	 //method return type is integer
 	public int empCheck() {
 		int empHour = 0;
 		int empCheck = (int) ((Math.random() * 10) % 3);
-
-		switch (empCheck) {
-		case ISFULLTIME:
+		switch (empCheck)  {
+		case IS_FULL_TIME:
 			System.out.println("Employee is Present for Full Time");
 			empHour = 8;
 			break;
 
-		case ISPARTTIME:
+		case IS_PART_TIME:
 			System.out.println("Employee is Present for Part Time");
 			empHour = 4;
 			break;
@@ -42,8 +51,8 @@ public class EmployeeWage
 	}
 
 	public static void main(String[] args) {
-		EmployeeWage emp = new EmployeeWage(20);
+		EmployeeWage emp = new EmployeeWage(20, 20);
 		emp.empWage();
 
-	}	  
+	}
 }
